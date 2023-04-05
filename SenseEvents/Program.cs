1d4a5a665e8d182855f732d1cf0b34407e0ad8b3
zar4za <zar4z4@yandex.ru>
2023-04-05 11:11:13 +0300

    feat: add mediatr service

--- SenseEvents/Program.cs
+++ SenseEvents/Program.cs
@@ -4,6 +4,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
