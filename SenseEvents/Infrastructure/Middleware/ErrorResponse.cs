50b5cdbf24f5b977234080507304d7b7067f217e
zar4za <zar4z4@yandex.ru>
2023-04-05 20:35:08 +0300

    feat: error handling

---
+++ SenseEvents/Infrastructure/Middleware/ErrorResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace SenseEvents.Infrastructure.Middleware
+{
+    public class ErrorResponse
+    {
+        public string Message { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
---
+++ SenseEvents/Infrastructure/Middleware/ValidationExceptionHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using FluentValidation;
+
+namespace SenseEvents.Infrastructure.Middleware
+{
+    public class ValidationExceptionHandlingMiddleware : IMiddleware
+    {
+        private const int ValidationExceptionStatusCode = 400;
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception e)
+            {
+                await HandleExceptionAsync(context, e);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext httpContext, Exception exception)
+        {
+            if (exception is not ValidationException validationException) throw exception;
+
+            httpContext.Response.StatusCode = ValidationExceptionStatusCode;
+
+            var response = new ErrorResponse()
+            {
+                Message = validationException.Message,
+                Errors = validationException.Errors.Select(e => e.ToString())
+            };
+
+            await httpContext.Response.WriteAsJsonAsync(response);
+        }
+    }
+}
