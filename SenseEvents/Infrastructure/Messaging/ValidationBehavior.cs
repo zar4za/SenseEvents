f438455c333b74daaac514bb3e4dca955ddac74d
zar4za <zar4z4@yandex.ru>
2023-04-05 18:56:34 +0300

    feat: addEvent validator registration

---
+++ SenseEvents/Infrastructure/Messaging/ValidationBehavior.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace SenseEvents.Infrastructure.Messaging
+{
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : class, ICommand<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var context = new ValidationContext<TRequest>(request);
+            var failures = _validators
+                .Select(v => v.Validate(context))
+                .SelectMany(result => result.Errors)
+                .Where(f => f != null)
+                .ToList();
+
+            if (failures.Any())
+            {
+                throw new ValidationException("VALIDATION EXCEPTION");
+            }
+
+            return await next();
+        }
+    }
+}
--- SenseEvents/Program.cs
+++ SenseEvents/Program.cs
@@ -1,14 +1,24 @@
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Hosting;
+using SenseEvents.Features.Events;
+using SenseEvents.Features.Id;
+using SenseEvents.Infrastructure.Messaging;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddMediatR(cfg =>
-{
-    cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
-});
 
+//singleton т.к. сервисы - заглушки
+builder.Services.AddSingleton<IGuidService, GuidService>();
+builder.Services.AddSingleton<IEventsService, EventsServiceMock>();
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly, ServiceLifetime.Transient);
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
