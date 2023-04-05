d8168fb342d1c415cb8a0cd76ca228662390729b
zar4za <zar4z4@yandex.ru>
2023-04-05 12:08:45 +0300

    feat: cqrs abstraction
    Интерфейсы для абстракции CQRS

---
+++ SenseEvents/Infrastructure/Messaging/ICommandHandler.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace SenseEvents.Infrastructure.Messaging
+{
+    public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse>
+        where TCommand : ICommand<TResponse>
+    {
+    }
+}
