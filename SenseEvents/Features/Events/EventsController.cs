70c3113d75102daf916579d1a5432fe376f8ac86
zar4za <zar4z4@yandex.ru>
2023-04-05 11:06:02 +0300

    feat: rest http interface for events
    Эндпойнты для REST API без их реализации.

---
+++ SenseEvents/Features/Events/EventsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SenseEvents.Features.Events
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id}")]
+        public void Put(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
