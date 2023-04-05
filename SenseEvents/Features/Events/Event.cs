1f928824347b0829d687128e9e1c718786c40066
zar4za <zar4z4@yandex.ru>
2023-04-05 11:14:23 +0300

    feat: event model
    Модель мероприятия

---
+++ SenseEvents/Features/Events/Event.cs
@@ -0,0 +1,19 @@
+﻿namespace SenseEvents.Features.Events
+{
+    public class Event
+    {
+        public Guid Id { get; set; }
+
+        public DateTime StartUtc { get; set; }
+
+        public DateTime EndUtc { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public Guid ImageId { get; set; }
+
+        public Guid SpaceId { get; set; }
+    }
+}
