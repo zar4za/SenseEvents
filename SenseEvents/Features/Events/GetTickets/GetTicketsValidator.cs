a7de5021a1ef814a7c0fdef2d0cdd2d07b98b015
zar4za <zar4z4@yandex.ru>
2023-04-08 23:40:39 +0300

    fix: update validators

---
+++ SenseEvents/Features/Events/GetTickets/GetTicketsValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace SenseEvents.Features.Events.GetTickets
+{
+    public class GetTicketsValidator : AbstractValidator<GetTicketsQuery>
+    {
+        public GetTicketsValidator()
+        {
+            RuleFor(x => x.EventId).NotEmpty();
+        }
+    }
+}
--- SenseEvents/Features/Events/UpdateEvent/UpdateEventValidator.cs
+++ SenseEvents/Features/Events/UpdateEvent/UpdateEventValidator.cs
@@ -5,12 +5,9 @@ namespace SenseEvents.Features.Events.UpdateEvent
 {
     public class UpdateEventValidator : AbstractValidator<UpdateEventCommand>
     {
-        public UpdateEventValidator(IEventsService events, IImageService images, ISpaceService spaces)
+        public UpdateEventValidator(IImageService images, ISpaceService spaces)
         {
-            RuleFor(e => e.Id)
-                .NotEmpty()
-                .Must(e => events.GetEvent(e).Result is not null )
-                .WithMessage(e => $"Мероприятия с Id '{e}' не существует.");
+            RuleFor(e => e.Id).NotEmpty();
             RuleFor(e => e.StartUtc).NotEmpty();
             RuleFor(e => e.EndUtc)
                 .NotEmpty()
