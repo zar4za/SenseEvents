a020cb227129a5d6e2c310e64dfdbd290c1a40b9
zar4za <zar4z4@yandex.ru>
2023-04-08 23:44:37 +0300

    feat: add ticket validation

---
+++ SenseEvents/Features/Events/AddTicket/AddTicketValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace SenseEvents.Features.Events.AddTicket
+{
+    public class AddTicketValidator : AbstractValidator<AddTicketCommand>
+    {
+        public AddTicketValidator()
+        {
+            RuleFor(x => x.EventId).NotEmpty();
+            RuleFor(x => x.OwnerId).NotEmpty();
+        }
+    }
+}
