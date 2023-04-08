6ce908c53e84bce2b944ca84cf4125aa8971d26f
zar4za <zar4z4@yandex.ru>
2023-04-08 23:40:39 +0300

    fix: update validators

--- SenseEvents/Features/Events/DeleteEvent/DeleteEventValidator.cs
+++ SenseEvents/Features/Events/DeleteEvent/DeleteEventValidator.cs
@@ -7,9 +7,7 @@ namespace SenseEvents.Features.Events.DeleteEvent
         public DeleteEventValidator(IEventsService events)
         {
             RuleFor(e => e.Id)
-                .NotEmpty()
-                .Must(e => events.GetEvent(e).Result is not null)
-                .WithMessage(e => $"Мероприятия с Id '{e}' не существует.");
+                .NotEmpty();
         }
     }
 }
