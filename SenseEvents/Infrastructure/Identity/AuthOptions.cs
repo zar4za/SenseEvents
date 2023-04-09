256d767226eeb274cb4046158b59b179cc71cd27
zar4za <zar4z4@yandex.ru>
2023-04-10 01:50:31 +0300

    fix: remove fake controller from swagger

---
+++ SenseEvents/Infrastructure/Identity/AuthOptions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace SenseEvents.Infrastructure.Identity
+{
+    public class AuthOptions
+    {
+        public const string ConfigSection = "Auth";
+
+        public string Audience { get; init; } = null!;
+
+        public string Authority { get; init; } = null!;
+
+        public string SecurityKey { get; init; } = null!;
+
+        public SecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecurityKey));
+        }
+    }
+}
