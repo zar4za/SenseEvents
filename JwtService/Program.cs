22e018b998d31775bc25244a8074f7fd9676a167
zar4za <zar4z4@yandex.ru>
2023-04-12 16:44:27 +0300

    feat: docker compose pipeline

--- JwtService/Program.cs
+++ JwtService/Program.cs
@@ -21,12 +21,8 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseCors("AllowAny");
 app.UseAuthorization();
 app.MapControllers();
