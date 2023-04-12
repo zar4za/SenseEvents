3b3824a42e92515fa197454455a3697ffa4f7e1d
zar4za <zar4z4@yandex.ru>
2023-04-12 11:50:53 +0300

    test: add ticket when valid

---
+++ SenseEvents.Tests/Features/Events/AddTicket/AddTicketTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NuGet.Frameworks;
+using SenseEvents.Features.Events;
+using SenseEvents.Features.Events.AddTicket;
+using SenseEvents.Infrastructure.Identity;
+
+namespace SenseEvents.Tests.Features.Events.AddTicket
+{
+    [TestFixture]
+    internal class AddTicketTests
+    {
+        private Guid _ticketGuid;
+        private IGuidService _guidService;
+        private IEventsService _eventsService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _ticketGuid = Guid.NewGuid();
+            var guidService = new Mock<IGuidService>();
+            guidService
+                .Setup(x => x.GetNewId())
+                .Returns(_ticketGuid);
+
+            _guidService = guidService.Object;
+        }
+
+
+        [Test]
+        public async Task Handle_EventExists_ShouldAddTicket()
+        {
+            var ticketId = Guid.NewGuid();
+            var eventId = Guid.NewGuid();
+            var ownerId = Guid.NewGuid();
+            var command = new AddTicketCommand
+            {
+                EventId = eventId,
+                OwnerId = ownerId
+            };
+
+            var ticket = new Ticket
+            {
+                Id = _ticketGuid,
+                OwnerId = ownerId,
+                Seat = 1
+            };
+
+            var eventsService = new Mock<IEventsService>();
+            eventsService
+                .Setup(x => x.AddTicket(eventId, ticket))
+                .ReturnsAsync(ticket);
+
+            var handler = new AddTicketHandler(_guidService, eventsService.Object);
+
+
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Ticket.Id, Is.EqualTo(ticket.Id));
+        }
+    }
+}
