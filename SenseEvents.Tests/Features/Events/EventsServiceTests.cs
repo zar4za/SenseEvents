544357dae8fe8632c943e2a1e44b7c19260a122e
zar4za <zar4z4@yandex.ru>
2023-04-12 13:10:20 +0300

    test: event service mongo tests

---
+++ SenseEvents.Tests/Features/Events/EventsServiceTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Mongo2Go;
+using SenseEvents.Features.Events;
+using SenseEvents.Infrastructure.Identity;
+
+namespace SenseEvents.Tests.Features.Events
+{
+    [TestFixture]
+    internal class EventsServiceTests : IDisposable
+    {
+        private Guid _eventId;
+        private MongoDbRunner _runner;
+        private IEventsService _eventsService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _runner = MongoDbRunner.StartForDebugging();
+            var options = new EventsMongoOptions
+            {
+                ConnectionString = _runner.ConnectionString,
+                EventsCollectionName = "events",
+                DatabaseName = "sense-test"
+            };
+            var guidService = new Mock<IGuidService>();
+            guidService.Setup(x => x.GetNewId())
+                .Returns(Guid.NewGuid());
+
+
+            _eventsService = new EventsService(Options.Create(options), guidService.Object);
+        }
+
+        [Test]
+        public async Task AddEvent_ValidEvent_ReturnsAddedId()
+        {
+            var id = await _eventsService.AddEvent(new Event()
+            {
+                Description = "test",
+                EndUtc = DateTime.UtcNow.AddDays(2),
+                Id = Guid.NewGuid(),
+                ImageId = Guid.NewGuid(),
+                MaxTickets = 0,
+                Name = "test",
+                SpaceId = Guid.NewGuid(),
+                StartUtc = DateTime.UtcNow,
+                Tickets = new List<Ticket>()
+            });
+
+            Assert.That(id, Is.Not.Empty);
+            _eventId = id;
+        }
+
+        [Test]
+        public void AddTicket_ReachedLimit_ThrowsInvalidOperationException()
+        {
+            Assert.ThrowsAsync<InvalidOperationException>(async () =>
+            {
+                await _eventsService.AddTicket(_eventId, new Ticket()
+                {
+                    Id = Guid.NewGuid(),
+                    OwnerId = Guid.NewGuid(),
+                    Seat = 1
+                });
+            });
+        }
+
+        [Test]
+        public async Task RemoveTicket_CorrectId_ReturnsTrue()
+        {
+            var success = await _eventsService.DeleteEvent(_eventId);
+            Assert.That(success, Is.True);
+        }
+
+
+        public void Dispose()
+        {
+            _runner.Dispose();
+        }
+    }
+}
