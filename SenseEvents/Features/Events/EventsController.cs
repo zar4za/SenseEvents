1f0ab715794196e96e7a6bc668bff9272421aa71
zar4za <zar4z4@yandex.ru>
2023-04-18 17:48:55 +0300

    feat: http logging

--- SenseEvents/Features/Events/EventsController.cs
+++ SenseEvents/Features/Events/EventsController.cs
@@ -17,9 +17,11 @@ namespace SenseEvents.Features.Events;
 public class EventsController : ControllerBase
 {
     private readonly IMediator _mediator;
+    private readonly ILogger<EventsController> _logger;
 
-    public EventsController(IMediator mediator)
+    public EventsController(ILogger<EventsController> logger, IMediator mediator)
     {
+        _logger = logger;
         _mediator = mediator;
     }
 
@@ -32,6 +34,7 @@ public class EventsController : ControllerBase
     [ProducesResponseType(statusCode: 401)]
     public async Task<IActionResult> GetEvents()
     {
+        _logger.LogInformation($"Received GET /api/events");
         var events = await _mediator.Send(new GetEventsQuery());
         return Ok(events);
     }
@@ -47,6 +50,7 @@ public class EventsController : ControllerBase
     [ProducesResponseType(statusCode: 401)]
     public async Task<IActionResult> AddEvent([FromBody] AddEventCommand command)
     {
+        _logger.LogInformation($"Received POST /api/events");
         var eventId = await _mediator.Send(command);
         return Ok(eventId);
     }
@@ -66,6 +70,7 @@ public class EventsController : ControllerBase
     public async Task<IActionResult> UpdateEvent(Guid id, [FromBody] UpdateEventCommand command)
     {
         command.Id = id;
+        _logger.LogInformation($"Received PUT /api/events");
         var success = await _mediator.Send(command);
         return Ok(success);
     }
@@ -83,6 +88,7 @@ public class EventsController : ControllerBase
     // ReSharper disable once RouteTemplates.MethodMissingRouteParameters
     public async Task<IActionResult> DeleteEvent([FromQuery] DeleteEventCommand command)
     {
+        _logger.LogInformation($"Received DELETE /api/events");
         var success = await _mediator.Send(command);
         return Ok(success);
     }
@@ -101,6 +107,7 @@ public class EventsController : ControllerBase
     // ReSharper disable once RouteTemplates.MethodMissingRouteParameters
     public async Task<IActionResult> GetTickets([FromQuery] GetTicketsQuery query)
     {
+        _logger.LogInformation($"Received GET /api/events/{query.EventId}/tickets");
         var tickets = await _mediator.Send(query);
         return Ok(tickets);
     }
@@ -120,6 +127,7 @@ public class EventsController : ControllerBase
     public async Task<IActionResult> AddTicket(Guid id, AddTicketCommand command)
     {
         command.EventId = id;
+        _logger.LogInformation($"Received GET /api/events/{id}/tickets");
         var ticket = await _mediator.Send(command);
         return Ok(ticket);
     }
--- SenseEvents/Infrastructure/Services/Images/ImageHttpService.cs
+++ SenseEvents/Infrastructure/Services/Images/ImageHttpService.cs
@@ -7,9 +7,11 @@ public class ImageHttpService : IImageService
 {
     private readonly HttpClient _httpClient;
     private readonly ServiceOptions _options;
+    private readonly ILogger<ImageHttpService> _logger;
 
-    public ImageHttpService(HttpClient client, IOptions<ServiceOptions> options)
+    public ImageHttpService(ILogger<ImageHttpService> logger, HttpClient client, IOptions<ServiceOptions> options)
     {
+        _logger = logger;
         _httpClient = client;
         _options = options.Value;
     }
@@ -18,6 +20,8 @@ public class ImageHttpService : IImageService
     {
         var uri = _options.ImageServiceUrl + $"/{id}";
         _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(_options.ApiToken);
+
+        _logger.LogInformation($"Sending request to {uri}");
         var response = _httpClient.GetFromJsonAsync<ImageResponse>(uri).Result; 
         return response?.Exists == true;
     }
