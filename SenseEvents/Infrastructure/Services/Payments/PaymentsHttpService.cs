66aab3eed491186a8c787ae8cf6e5147b99cf836
zar4za <zar4z4@yandex.ru>
2023-04-18 15:10:13 +0300

    fix: api endpoint and json parsing

--- SenseEvents/Infrastructure/Services/Payments/PaymentsHttpService.cs
+++ SenseEvents/Infrastructure/Services/Payments/PaymentsHttpService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Options;
+﻿using System.Net.Http.Headers;
+using Microsoft.Extensions.Options;
 using System.Text.Json;
 using PaymentsService.Shared;
 using PaymentsService.Shared.AddPayment;
@@ -9,34 +10,40 @@ public class PaymentsHttpService : IPaymentsService
 {
     private readonly HttpClient _httpClient;
     private readonly ServiceOptions _options;
+    private readonly JsonSerializerOptions _serializerOptions;
 
     public PaymentsHttpService(HttpClient client, IOptions<ServiceOptions> options)
     {
         _httpClient = client;
         _options = options.Value;
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(options.Value.ApiToken);
+        _serializerOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
     }
 
     public async Task<Payment> Create(AddPaymentCommand command)
     {
-        var uri = _options.PaymentsServiceUrl + "/api/payments";
+        var uri = _options.PaymentsServiceUrl;
         var response = await _httpClient.PostAsync(uri, JsonContent.Create(command));
-        var payment = JsonSerializer.Deserialize<Payment>(await response.Content.ReadAsStreamAsync());
+        var payment = JsonSerializer.Deserialize<Payment>(await response.Content.ReadAsStreamAsync(), _serializerOptions);
         return payment!;
     }
 
     public async Task<Payment> Confirm(Guid id)
     {
-        var uri = _options.PaymentsServiceUrl + $"/api/payments/{id}/confirm";
+        var uri = _options.PaymentsServiceUrl + $"/{id}/confirm";
         var response = await _httpClient.PutAsync(uri, null);
-        var payment = JsonSerializer.Deserialize<Payment>(await response.Content.ReadAsStreamAsync());
+        var payment = JsonSerializer.Deserialize<Payment>(await response.Content.ReadAsStreamAsync(), _serializerOptions);
         return payment!;
     }
 
     public async Task<Payment> Cancel(Guid id)
     {
-        var uri = _options.PaymentsServiceUrl + $"/api/payments/{id}/cancel";
+        var uri = _options.PaymentsServiceUrl + $"/{id}/cancel";
         var response = await _httpClient.PutAsync(uri, null);
-        var payment = JsonSerializer.Deserialize<Payment>(await response.Content.ReadAsStreamAsync());
+        var payment = JsonSerializer.Deserialize<Payment>(await response.Content.ReadAsStreamAsync(), _serializerOptions);
         return payment!;
     }
 }
