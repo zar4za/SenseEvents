d085649ec48a94dd7065ea9038b02e95dcc2495d
zar4za <zar4z4@yandex.ru>
2023-04-23 10:52:18 +0300

    feat: reaplace HttpClient with IHttpClientFactory

--- SenseEvents/Infrastructure/Services/Spaces/SpaceHttpService.cs
+++ SenseEvents/Infrastructure/Services/Spaces/SpaceHttpService.cs
@@ -1,24 +1,23 @@
 ﻿using Microsoft.Extensions.Options;
-using System.Net.Http.Headers;
 
 namespace SenseEvents.Infrastructure.Services.Spaces;
 
 public class SpaceHttpService : ISpaceService
 {
-    private readonly HttpClient _httpClient;
+    private readonly IHttpClientFactory _clientFactory;
     private readonly ServiceOptions _options;
 
-    public SpaceHttpService(HttpClient client, IOptions<ServiceOptions> options)
+    public SpaceHttpService(IHttpClientFactory factory, IOptions<ServiceOptions> options)
     {
-        _httpClient = client;
+        _clientFactory = factory;
         _options = options.Value;
     }
 
     public bool SpaceExists(Guid id)
     {
-        var uri = _options.SpaceServiceUrl + $"/{id}";
-        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(_options.ApiToken);
-        var response = _httpClient.GetFromJsonAsync<SpaceResponse>(uri).Result; 
+        using var client = _clientFactory.CreateClient(ServiceOptions.HttpClientName);
+        var uri = $"{_options.SpaceServiceUrl}/{id}";
+        var response = client.GetFromJsonAsync<SpaceResponse>(uri).Result; 
         return response?.Exists == true;
     }
 }
