ba786606ec6e4b5fd19f8e6fa1fb6b67da1780fa
zar4za <zar4z4@yandex.ru>
2023-04-24 00:12:48 +0300

    style: resharper notations

--- SenseEvents/Infrastructure/RabbitMQ/Events/ImageDelete/ImageDeleteConsumer.cs
+++ SenseEvents/Infrastructure/RabbitMQ/Events/ImageDelete/ImageDeleteConsumer.cs
@@ -1,20 +1,19 @@
 ﻿using MassTransit;
 using SenseEvents.Features.Events;
 
-namespace SenseEvents.Infrastructure.RabbitMQ.Events.ImageDelete
+namespace SenseEvents.Infrastructure.RabbitMQ.Events.ImageDelete;
+
+public class ImageDeleteConsumer : IConsumer<ImageDeleteEvent>
 {
-    public class ImageDeleteConsumer : IConsumer<ImageDeleteEvent>
+    private readonly IEventsService _eventsService;
+
+    public ImageDeleteConsumer(IEventsService eventsService)
     {
-        private readonly IEventsService _eventsService;
+        _eventsService = eventsService;
+    }
 
-        public ImageDeleteConsumer(IEventsService eventsService)
-        {
-            _eventsService = eventsService;
-        }
-
-        public async Task Consume(ConsumeContext<ImageDeleteEvent> context)
-        {
-            await _eventsService.UpdateImage(context.Message.ImageId, null);
-        }
+    public async Task Consume(ConsumeContext<ImageDeleteEvent> context)
+    {
+        await _eventsService.UpdateImage(context.Message.ImageId, null);
     }
 }
--- SenseEvents/Infrastructure/RabbitMQ/RabbitMqOptions.cs
+++ SenseEvents/Infrastructure/RabbitMQ/RabbitMqOptions.cs
@@ -6,8 +6,6 @@ public class RabbitMqOptions
 
     public string Host { get; set; } = null!;
 
-    public string Queue { get; set; } = null!;
-
     public string Username { get; set; } = null!;
 
     public string Password { get; set; } = null!;
